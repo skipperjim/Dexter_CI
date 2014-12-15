5d609c595ee8a696a9b6208c333949471db0a37a
Steven Gray <sgrayjr289@gmail.com>
2014-12-15 08:47:59 -0700

    Updated config files and added .gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/dexter_ci';
+$config['base_url']	= 'http://ci_dexter.it-gray';
 
 /*
 |--------------------------------------------------------------------------
